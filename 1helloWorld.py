04f3a3292ca9a30193473d90209ddd4b24479cbc
Anjali Khot <anjalikhot5268@gmail.com>
2024-07-30 15:30:15 +0530

    Initial commit

---
+++ 1helloWorld.py
@@ -0,0 +1,4 @@
+#First prg to print "Hello World"
+print("Hello World") #This print statement actually prints the given statement and give next line 
+print("Hi",end=" ") #This print statement prints Hi and gives a white space as given in the end paramenter
+print("Yo") #to demo the white space of earlier print statement
